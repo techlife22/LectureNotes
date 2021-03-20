306205272c3a27254c18289aa3241108451dd485
Brandon Beason <techlife@justcode.live>
2021-03-20 14:46:35 -0400

    20Mar update

---
+++ API-challenge-1/script.js
@@ -0,0 +1,3 @@
+// baseURL for the current F1 season schedule
+const baseURL = 'http://ergast.com/api/f1/2021';
+let URL; // to be used later 
--- objects/introToObjects.js
+++ objects/introToObjects.js
@@ -1,46 +1,52 @@
 let netflix = {
-    id: 1,
-    name: "The Office",
-    season1: {
-        seasonInfo: {
-            episodeInfo: [{
-                    episode: 1,
-                    episodeName: "Pilot"
-                },
-                {
-                    episode: 2,
-                    episodeName: "Diversity Day"
-                },
-                {
-                    episode: 3,
-                    episodeName: "Health Care"
-                },
-                {
-                    episode: 4,
-                    episodeName: "The Alliance"
-                },
-                {
-                    episode: 5,
-                    episodeName: "Basketball"
-                },
-                {
-                    episode: 6,
-                    episodeName: "Hot Girl"
-                },
-            ]
-        }
+  id: 1,
+  name: "The Office",
+  season1: {
+    seasonInfo: {
+      episodeInfo: [
+        {
+          episode: 1,
+          episodeName: "Pilot",
+        },
+        {
+          episode: 2,
+          episodeName: "Diversity Day",
+        },
+        {
+          episode: 3,
+          episodeName: "Health Care",
+        },
+        {
+          episode: 4,
+          episodeName: "The Alliance",
+        },
+        {
+          episode: 5,
+          episodeName: "Basketball",
+        },
+        {
+          episode: 6,
+          episodeName: "Hot Girl",
+        },
+      ],
     },
-    season2: {},
-    season3: {}
+  },
+  season2: {},
+  season3: {},
 };
 
 console.log("All Data: ", netflix);
 console.log("Just Season Info: ", netflix.season1.seasonInfo);
 console.log("Just episonde 4 ", netflix.season1.seasonInfo.episodeInfo[3]);
-console.log("Just episode name", netflix.season1.seasonInfo.episodeInfo[3].episodeName)
+console.log(
+  "Just episode name",
+  netflix.season1.seasonInfo.episodeInfo[3].episodeName
+);
 
-console.log("Episode 5 number and name: ", netflix.season1.seasonInfo.episodeInfo[4].episode);
-
+console.log(
+  "Episode 5 number and name: ",
+  netflix.season1.seasonInfo.episodeInfo[4].episode
+);
 
 /*
 -JSON stands for JavaScript Object Notation
@@ -53,31 +59,31 @@ Example JSON data- https://rickandmortyapi.com/api/character/
 */
 
 let spaceJam = {
-    toonSquad: {
-        human: 'Michael Jordan',
-        rabbit1: 'Bugs Bunny',
-        rabbit2: 'Lola Bunny',
-        duck: 'Daffy Duck',
-        tDevil: 'Tasmanian Devil',
-        bird: 'Tweety',
-        cat: 'Sylvester',
-        pig: 'Porky Pig'
-    },
-    monstars: {
-        0: 'Bupkus',
-        1: 'Bang',
-        2: 'Nawt',
-        3: 'Pound',
-        4: 'Blanko'
-    },
-    nbaPlayers: {
-        phoenixSuns: 'Charles Barkley',
-        newJerseyNets: 'Shawn Bradley',
-        newYorkNicks: 'Patrick Ewing',
-        charlotteHornets1: 'Larry Johnson',
-        charlotteHornets2: 'Muggsy Bogues'
-    }
-}
+  toonSquad: {
+    human: "Michael Jordan",
+    rabbit1: "Bugs Bunny",
+    rabbit2: "Lola Bunny",
+    duck: "Daffy Duck",
+    tDevil: "Tasmanian Devil",
+    bird: "Tweety",
+    cat: "Sylvester",
+    pig: "Porky Pig",
+  },
+  monstars: {
+    0: "Bupkus",
+    1: "Bang",
+    2: "Nawt",
+    3: "Pound",
+    4: "Blanko",
+  },
+  nbaPlayers: {
+    phoenixSuns: "Charles Barkley",
+    newJerseyNets: "Shawn Bradley",
+    newYorkNicks: "Patrick Ewing",
+    charlotteHornets1: "Larry Johnson",
+    charlotteHornets2: "Muggsy Bogues",
+  },
+};
 //object constructor
 console.log(Object.keys(spaceJam.monstars));
 //grab values
